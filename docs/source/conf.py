f1baa43362ca39a6e98d435d7e74f396bf4f97d7
Yasmin <ymkgz8@mail.missouri.edu>
2024-12-09 10:59:33 -0800

    Set up GitHub Pages deployment for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,48 +1,43 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'VASCilia'
 copyright = '2024, Yasmin Kassim'
 author = 'Yasmin Kassim'
 release = '1.3.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_book_theme'
-html_static_path = ['docs/_static']
-html_logo = "VASCilia_logo1.png"
-html_baseurl = "https://ucsdmanorlab.github.io/Napari-VASCilia/"
-html_css_files = [
-    "styles/sphinx-book-theme.css",
-    "styles/theme.css",
-    "pygments.css",
-    "custom.css",
+extensions = [
+    "sphinx.ext.autodoc",      # Auto-generate documentation from docstrings
+    "sphinx.ext.viewcode",     # Add links to source code
+    "sphinx.ext.githubpages"   # Ensure compatibility with GitHub Pages
 ]
 
+templates_path = ['_templates']  # Path for custom templates
+exclude_patterns = []           # Patterns to exclude from the build
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']  # Path for static assets (CSS, images, JS)
+html_logo = "_static/VASCilia_logo1.png"  # Logo file
+
+# GitHub Pages base URL
+html_baseurl = "https://ucsdmanorlab.github.io/Napari-VASCilia/"
+
+# Include custom CSS files
+html_css_files = [
+    "custom.css"  # Your custom styles
+]
+
+# Theme options for sphinx_book_theme
 html_theme_options = {
     "repository_url": "https://github.com/ucsdmanorlab/Napari-VASCilia",
     "use_repository_button": True,
     "use_download_button": True,
     "use_fullscreen_button": True,
     "show_navbar_depth": 2,
-    "logo": {
-        "image_light": "_static/VASCilia_logo1.png",
-        "image_dark": "_static/VASCilia_logo1.png",
-    },
 }
+
+# -- Custom settings for GitHub Actions -------------------------------------
+# Ensure the path to images inside the `features/_static` folder works
+html_extra_path = ['features/_static']
